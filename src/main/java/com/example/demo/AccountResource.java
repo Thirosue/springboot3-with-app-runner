31446e92949c86e63d1e364c38d565feef9a107a
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2025-01-18 20:49:36 +0900

    feat: add no db version

--- src/main/java/com/example/demo/AccountResource.java
+++ src/main/java/com/example/demo/AccountResource.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,21 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/test")
 public class AccountResource {
-  private final AccountRepository accountRepository;
-
-  @Autowired
-  public AccountResource(AccountRepository accountRepository) {
-    this.accountRepository = accountRepository;
-  }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Account> getUserById(@PathVariable Long id) {
-    Account account =
-        accountRepository
-            .findById(id)
-            .orElseThrow(() -> new RuntimeException("User not found with id " + id));
-    return ResponseEntity.ok(account);
+  public ResponseEntity<String> getUserById(@PathVariable Long id) {
+    return ResponseEntity.ok("test");
   }
 }
